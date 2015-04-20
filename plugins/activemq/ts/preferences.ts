397fc711ea2b1f298e011cde66d8ab79b4e03719
Claus Ibsen <claus.ibsen@gmail.com>
2015-04-20 14:31:23 +0200

    #18: ActiveMQ and Camel plugin - Preference should use patternfly style

--- plugins/activemq/ts/preferences.ts
+++ plugins/activemq/ts/preferences.ts
@@ -8,17 +8,48 @@
 module ActiveMQ {
   _module.controller("ActiveMQ.PreferencesController", ["$scope", "localStorage", "userDetails", "$rootScope", ($scope, localStorage, userDetails, $rootScope) => {
 
+    var config = {
+      properties: {
+        activemqUserName: {
+          type: 'string',
+          description: 'The user name to be used when connecting to the broker'
+        },
+        activemqPassword: {
+          type: 'string',
+          description: 'Password to be used when connecting to the broker'
+        },
+        activemqFilterAdvisoryTopics: {
+          type: 'boolean',
+          default: 'false',
+          description: 'Whether to exclude advisory topics in tree/table'
+        },
+        activemqBrowseBytesMessages: {
+          type: 'number',
+          enum: {
+            'Hex and text': 1,
+            'Decimal and text': 2,
+            'Hex': 4,
+            'Decimal': 8,
+            'Off': 99
+          },
+          description: 'Browsing byte messages should display the message body as'
+        }
+      }
+    };
+
+    $scope.entity = $scope;
+    $scope.config = config;
+
     Core.initPreferenceScope($scope, localStorage, {
         'activemqUserName': {
-          'value': userDetails.username,
+          'value': userDetails.username ? userDetails.username : ""
         },
         'activemqPassword': {
-          'value': userDetails.password
+          'value': userDetails.password ? userDetails.password : ""
         },
         'activemqBrowseBytesMessages': {
           'value': 1,
-          'converter': parseInt,
-          'formatter': (value) => { return "" + value }
+          'converter': parseInt
         },
         'activemqFilterAdvisoryTopics': {
           'value': false,
